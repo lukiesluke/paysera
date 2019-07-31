76795fe70ada845f0e54ca09e009d1e1abfbad42
logarces <Luke.O.Garces.-ND@disney.com>
2019-07-31 08:14:15 +0800

    remove log

--- app/src/main/java/com/lwg/paysera/interactor/PayInteractor.kt
+++ app/src/main/java/com/lwg/paysera/interactor/PayInteractor.kt
@@ -1,6 +1,5 @@
 package com.lwg.paysera.interactor
 
-import android.util.Log
 import com.lwg.paysera.callbacks.RepositoryCallbacks
 import com.lwg.paysera.model.Amount
 import com.lwg.paysera.model.SellCurrency
@@ -14,24 +13,19 @@ class PayInteractor : PayInteractorView {
     override fun onConvertCurrency(sell: SellCurrency?, callback: RepositoryCallbacks.IResponseCallback<Amount>) {
 
         val call: Call<Amount> = ApiClient.getClient.onCurrencyConverter(sell?.fromAmount, sell?.fromCurrency, sell?.toCurrency)
-        Log.d("lwg", "onConvertCurrencyResponse sell?.fromAmount: " + sell?.fromAmount)
 
         call.enqueue(object : Callback<Amount> {
 
             override fun onResponse(call: Call<Amount>, response: Response<Amount>) {
                 if (response.isSuccessful) {
-                    Log.d("lwg", "onResponse " + response.body()?.amount!!.toDouble())
-                    Log.d("lwg", "onResponse " + response.body()?.currency)
                     val amount: Amount = response.body()!!
                     callback.onSuccess(amount)
                 } else {
                     callback.onError(response.message())
-                    Log.d("lwg", "error really" + response.message())
                 }
             }
 
             override fun onFailure(call: Call<Amount>, t: Throwable) {
-                Log.d("lwg", "errorT " + t.message)
                 callback.onError(t.message)
             }
         })
